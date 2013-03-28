192fa65d4e10db051ba822f3271c43e6e1a64452
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2013-03-27 22:27:02 -0300

    added records attributes to whitelist

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,5 +1,5 @@
 class Domain < ActiveRecord::Base
-  attr_accessible :dns1, :dns2, :name
+  attr_accessible :dns1, :dns2, :name, :records_attributes
 
   has_many :records
   accepts_nested_attributes_for :records
