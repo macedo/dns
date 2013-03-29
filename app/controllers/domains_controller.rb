0c0389cdc245714589b951cea5e4841a92ee764c
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2013-03-29 10:48:59 -0300

    Refactor controllers

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,84 +1,43 @@
 class DomainsController < ApplicationController
-  # GET /domains
-  # GET /domains.json
+  before_filter :find_domain, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @domains = Domain.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @domains }
-    end
+    respond_with(@domains)
   end
 
-  # GET /domains/1
-  # GET /domains/1.json
   def show
-    @domain = Domain.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @domain }
-    end
+    respond_with(@domain)
   end
 
-  # GET /domains/new
-  # GET /domains/new.json
   def new
-    @domain = Domain.new
-    @domain.records.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @domain }
+    @domain = Domain.new do |domain|
+      domain.records.build
     end
   end
 
-  # GET /domains/1/edit
   def edit
-    @domain = Domain.find(params[:id])
   end
 
-  # POST /domains
-  # POST /domains.json
   def create
     @domain = Domain.new(params[:domain])
-
-    respond_to do |format|
-      if @domain.save
-        format.html { redirect_to @domain, notice: 'Domain was successfully created.' }
-        format.json { render json: @domain, status: :created, location: @domain }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @domain.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Domain was successfully created.' if @domain.save
+    respond_with(@domain)
   end
 
-  # PUT /domains/1
-  # PUT /domains/1.json
   def update
-    @domain = Domain.find(params[:id])
-
-    respond_to do |format|
-      if @domain.update_attributes(params[:domain])
-        format.html { redirect_to @domain, notice: 'Domain was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @domain.errors, status: :unprocessable_entity }
-      end
-    end
+    @domain.update_attributes(params[:domain])
+    flash[:notice] = 'Domain was successfully updated.' if @domain.update_attributes(params[:domain])
+    respond_with(@domain)
   end
 
-  # DELETE /domains/1
-  # DELETE /domains/1.json
   def destroy
-    @domain = Domain.find(params[:id])
     @domain.destroy
+    respond_with(@domain)
+  end
 
-    respond_to do |format|
-      format.html { redirect_to domains_url }
-      format.json { head :no_content }
-    end
+  private
+  def find_domain
+    @domain = Domain.find(params[:id])
   end
 end
